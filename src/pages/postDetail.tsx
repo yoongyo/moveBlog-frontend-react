a89e426c21a8501e76ad702f2f31429054ff138e
yoongyo <33406406+yoongyo@users.noreply.github.com>
2021-09-01 13:17:52 +0900

    test

--- src/pages/postDetail.tsx
+++ src/pages/postDetail.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Header from '../component/header';
-import { FacebookProvider, Comments } from 'react-facebook';
 
 
 export const postDetail =  () => {
@@ -10,11 +9,6 @@ export const postDetail =  () => {
             <div className="max-w-4xl mx-auto my-8">
                 <div className="border-b">
                     <h1 className="font-bold text-3xl py-5">우아한테크러닝 4기 후기</h1>
-                </div>
-                <div>
-                    <FacebookProvider appId="123456789">
-                        <Comments href="http://www.facebook.com" />
-                    </FacebookProvider>
                 </div>
             </div>
         </div>
