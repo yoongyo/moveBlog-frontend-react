24a8ddd9b05e713aa235f0f3c3da49f1db028473
yoongyo <33406406+yoongyo@users.noreply.github.com>
2021-09-04 03:14:03 +0900

    add category function

--- src/component/addCategory.tsx
+++ src/component/addCategory.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { BACKEND_URL } from '../api/backendURL';
 import { useHistory } from 'react-router-dom';
 
 
 export const AddCategory  = () => {
-    
+    const [categoryName, setCategoryName] = useState("");
     let history = useHistory();
 
 
@@ -14,11 +14,11 @@ export const AddCategory  = () => {
             headers: {
                 'Content-Type': 'application/json',
             },
-            // body: JSON.stringify({name: name})
+            body: JSON.stringify({name: setCategoryName})
         })
         .then(res => {
-            console.log(res)
-            history.push('/')
+            // 전역적으로 category 추가
+            window.close();
         })
     }
 
@@ -28,10 +28,10 @@ export const AddCategory  = () => {
             <h1>추가할 카테고리명을 입력하세요.</h1>
             <div className="grid grid-cols-3 border-b py-8 px-2">
                 <div className="col-span-1">
-                    <h1 >카테고리명:</h1>
+                    <h1 className="">카테고리명:</h1>
                 </div>
                 <div className="col-span-2">
-                    <input className="border rounded-md w-full "/>
+                    <input className="border rounded-md w-full h-10 px-3"/>
                 </div>
             </div>
             <div style={{backgroundColor: "#f8f8f8"}} className="mt-8 p-4 flex flex-row-reverse">
