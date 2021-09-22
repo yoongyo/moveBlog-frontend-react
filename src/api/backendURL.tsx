56af3d9d25d8d3edcf8ea68a3e53d9f142aadc4d
yoongyo <33406406+yoongyo@users.noreply.github.com>
2021-09-22 23:41:52 +0900

    change backendurl

--- src/api/backendURL.tsx
+++ src/api/backendURL.tsx
@@ -1,3 +1,3 @@
-// export const BACKEND_URL = "https://blogs.move.is"
-export const BACKEND_URL = "http://localhost:8081"
+export const BACKEND_URL = "https://blogs.move.is"
+// export const BACKEND_URL = "http://localhost:8081"
 
