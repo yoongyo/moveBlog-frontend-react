74a0501a92c1edde45d3703e854788f02b08d00c
yoongyo <33406406+yoongyo@users.noreply.github.com>
2021-09-25 12:14:16 +0900

    Korea dateTime

--- src/component/dateTime/dateFormat.tsx
+++ src/component/dateTime/dateFormat.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 
 export const DateFormat = ({datetime}:any) => {
-    const dt = new Date(datetime);
+    const KR_TIME_DIFF = 9 * 60 * 60 * 1000;
+    const dateTime = new Date(datetime);    
+    const dt = new Date(dateTime.getTime() + KR_TIME_DIFF)
     const year = dt.getFullYear();
     const month = dt.getMonth()+1 > 10 ? dt.getMonth()+1: '0'+(dt.getMonth()+1);
     const date = dt.getDate() > 10 ? dt.getDate(): '0'+(dt.getDate());
--- src/component/dateTime/datetimeFormat.tsx
+++ src/component/dateTime/datetimeFormat.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 
 export const DateTimeFormat = ({datetime, text}:any) => {
-    const dt = new Date(datetime);
+    const KR_TIME_DIFF = 9 * 60 * 60 * 1000;
+    const dateTime = new Date(datetime);    
+    const dt = new Date(dateTime.getTime() + KR_TIME_DIFF)
     const year = dt.getFullYear();
-    const month = dt.getMonth()+1 > 10 ? dt.getMonth()+1: '0'+(dt.getMonth()+1);
+    const month = dt.getMonth() + 1 > 10 ? dt.getMonth()+1: '0'+(dt.getMonth()+1);
     const date = dt.getDate() > 10 ? dt.getDate(): '0'+(dt.getDate());
     const hour = dt.getHours() > 10 ? dt.getHours(): '0' + (dt.getHours());
     const minute = dt.getMinutes() > 10 ? + dt.getMinutes(): '0' + (dt.getMinutes());
